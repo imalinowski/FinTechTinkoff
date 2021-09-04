30b9fd3aa1a2cb76babdd20bf604559cde1a5b00
Ilya Malinowski <malinowski221106@gmail.com>
2021-09-04 21:26:04 +0300

    base design

---
+++ app/src/main/java/com/malinowski/memloader/Card.kt
@@ -0,0 +1,44 @@
+package com.malinowski.memloader
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun card(modifier: Modifier = Modifier, bitmap: ImageBitmap){
+    Image(
+        bitmap = bitmap,
+        contentDescription = "card",
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(15.dp))
+    )
+}
+
+@Preview
+@Composable
+fun cardPreview(){
+    card(bitmap = createImage(300,400, Color.Cyan))
+}
+fun createImage(width: Int, height: Int, color: Color): ImageBitmap {
+    val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    val paint = Paint()
+    paint.color = color.hashCode()
+    canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+    return bitmap.asImageBitmap()
+}
--- app/src/main/java/com/malinowski/memloader/MainActivity.kt
+++ app/src/main/java/com/malinowski/memloader/MainActivity.kt
@@ -3,11 +3,22 @@ package com.malinowski.memloader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Replay
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.malinowski.memloader.ui.theme.MemLoaderTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +26,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MemLoaderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MemScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MemScreen() {
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = "Developers Life",
+            modifier = Modifier,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        )
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+        ) {
+            val modifier = Modifier.weight(1.0f)
+            Text(text = "Последнее", modifier , textAlign = TextAlign.Center,)
+            Text(text = "Лучшее", modifier, textAlign = TextAlign.Center,)
+            Text(text = "Горячее", modifier, textAlign = TextAlign.Center,)
+        }
+        Box(modifier = Modifier.weight(2.0f)
+            .fillMaxSize()
+            .padding(10.dp)
+        ){
+            card(bitmap = createImage(300,300, Color.Yellow))
+        }
+        Row( Modifier ){
+            IconButton(onClick = { }) {
+                Icon(
+                    Icons.Rounded.Replay, "Icon",
+                    tint = MaterialTheme.colors.primaryVariant,
+                )
+            }
+            IconButton(onClick = { }) {
+                Icon(
+                    Icons.Rounded.ArrowForward, "Icon",
+                    tint = MaterialTheme.colors.primaryVariant,
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true
+)
 @Composable
-fun DefaultPreview() {
+fun defaultPreview() {
     MemLoaderTheme {
-        Greeting("Android")
+        MemScreen()
     }
 }
